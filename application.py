bf2c353a390536cf283b32ecd4bb051b69e564e2
Gabriel Vande Hei <gfvandehei@gmail.com>
2017-07-29 21:22:54 -0700

    added basic text input functionality

--- application.py
+++ application.py
@@ -1,12 +1,27 @@
 from Tkinter import *
 
 class WinComGUI(Frame):
+
+    def printInput(self):
+        '''
+        this works sort of, it wont print until the program is closed
+        '''
+        input_string=self.user_input.get()
+        print(input_string)
+        
     def createWidgets(self):
         #area for initializing components of GUI
         self.quit_button=Button(self)
         self.quit_button["text"]="Exit"
         self.quit_button["command"]=self.quit
         self.quit_button.pack()
+
+        self.user_input=Entry() #makes user input window area
+        self.user_input.pack()
+        self.input_button=Button(self)
+        self.input_button["text"]="Read Text"
+        self.input_button['command']=self.printInput
+        self.input_button.pack()
 
     def __init__(self,master=None):
         Frame.__init__(self, master)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Tkinter import *
 from application import WinComGUI
 
+
 if __name__=="__main__":
     root=Tk()
     mainapp=WinComGUI(master=root)
