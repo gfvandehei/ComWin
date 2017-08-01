34530e9affe77e2f79221361c1ab8bc391ca4db0
gabe <gvandehei@thinkom.com>
2017-08-01 16:42:43 -0700

    added working exit button

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import socket
 import threading
+import sys
+import os
 
 class WinComGUI(Frame):
     def recieve_data(self):
@@ -32,12 +34,20 @@ class WinComGUI(Frame):
             self.output.insert(INSERT, "you are not yet connected \n")
         else:
             self.c.send(bytes(userinput,"utf-8"))
+    
+    def exit(self):
+        
+        self.ClientSocket.close()
+        self.ServerSocket.close()        
+        os._exit(0)        
         
     def createWidgets(self):
         self.connect_button=Button( text="Connect", bg="red", command=self.connect_command)
         self.connect_button.grid(row=0, sticky=W)
         self.send_button=Button( text="Send     ",bg="green", command=self.send_command)
         self.send_button.grid(row=1, sticky=W)
+        self.exit_button=Button(text="Exit", command=self.exit)
+        self.exit_button.grid(row=0,sticky=E)
 
         self.ip_entry=Entry()
         self.ip_entry.grid(row=0, column=0,sticky=W, padx=80)
@@ -52,6 +62,8 @@ class WinComGUI(Frame):
             
 
     def __init__(self,master=None):
+        self.exitflag=0
+        self.master=master
         self.output_message=""
         Frame.__init__(self, master)
         host=socket.gethostbyname(socket.gethostname())
--- main.py
+++ main.py
@@ -3,20 +3,10 @@ from application import WinComGUI
 import socket
 
 if __name__=="__main__":
-    root=Tk()
-    #try to connect
-    '''
-    recsock=socket.socket()
-    try:
-        recsock.connect((socket.gethostname(),12345))
-        print(recsock.recv(1024))
-    except:
-        print("could not connect")'''
-    
-    
-    #end of connection
+    root=Tk()    
+
     mainapp=WinComGUI(master=root)
-    #mainapp.mainloop()
+
     while(True):
         mainapp.update_idletasks()
         mainapp.update()
